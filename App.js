5921903acd745a3fdc344424c76c2c886d52ef14
Pablo Pesce <papesce@gmail.com>
2017-10-20 13:32:52 -0300

    blinking state

--- App.js
+++ App.js
@@ -9,12 +9,33 @@ class Greeting extends Component {
     );
   }
 }
+
+class Blink extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {showText: true};
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
+  render() {
+    let display = this.state.showText ? this.props.text : '';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
          <Greeting name='Pablo'/>
          <Greeting name='Valeria'/>
+         <Blink text="Blinking!"/>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
