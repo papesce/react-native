8aed769db6b01716add6a0acc5f2c67c0cfabf4e
Pablo Pesce <papesce@gmail.com>
2017-10-20 12:31:35 -0300

    props sample

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+         <Greeting name='Pablo'/>
+         <Greeting name='Valeria'/>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
